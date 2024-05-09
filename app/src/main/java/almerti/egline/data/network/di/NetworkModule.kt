32187ac9655a5bf6abf6b9dcf3ca65ddac5eed8b
kovalev Andrey <andremae10@gmail.com>
2024-05-09 18:41:24 +0300

    add network module with di

---
+++ app/src/main/java/almerti/egline/data/network/di/NetworkModule.kt
@@ -0,0 +1,52 @@
+package almerti.egline.data.network.di
+
+
+import almerti.egline.data.network.RetrofitEglineNetworkApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule{
+
+    @Provides
+    @Singleton
+    fun okHttpCallClient():OkHttpClient {
+      return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor())
+            .build();
+        }
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://your-api-endpoint.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofitEglineNetworkApi(retrofit: Retrofit): RetrofitEglineNetworkApi {
+        return retrofit.create(RetrofitEglineNetworkApi::class.java)
+    }
+
+}
