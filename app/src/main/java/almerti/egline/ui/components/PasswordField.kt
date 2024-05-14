81752d37684446c9ae9a2a8713a60bc771000940
Dmitry <volikovd17@gmail.com>
2024-05-15 01:00:03 +0300

    User-auth branch from repository

---
+++ app/src/main/java/almerti/egline/ui/components/PasswordField.kt
@@ -0,0 +1,53 @@
+package almerti.egline.ui.components
+
+import almerti.egline.R
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PasswordField() {
+    var passwordVisibility: Boolean by remember {mutableStateOf(false)}
+    val inputText = remember {mutableStateOf("")}
+    val icon = remember {mutableIntStateOf(R.drawable.baseline_visibility_24)}
+
+    TextField(
+        modifier = Modifier.padding(bottom = 24.dp),
+        value = inputText.value,
+        placeholder = {Text(text = stringResource(id = R.string.password_field))},
+        visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+        trailingIcon = {
+            IconButton(
+                onClick = {
+                    passwordVisibility = !passwordVisibility
+                    if (passwordVisibility) {
+                        icon.intValue = R.drawable.baseline_visibility_off_24
+                    } else {
+                        icon.intValue = R.drawable.baseline_visibility_24
+                    }
+                },
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(icon.intValue),
+                    contentDescription = null,
+                )
+            }
+        },
+        onValueChange = {newText -> inputText.value = newText},
+    )
+}
