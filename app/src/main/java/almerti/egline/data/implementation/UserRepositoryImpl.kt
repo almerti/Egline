7fe6fa7540bd725fbb8d910b68e8533db3ffde5c
kovalev Andrey <andremae10@gmail.com>
2024-05-18 20:38:38 +0300

    Add logout function and improve logic and better loging

--- app/src/main/java/almerti/egline/data/implementation/UserRepositoryImpl.kt
+++ app/src/main/java/almerti/egline/data/implementation/UserRepositoryImpl.kt
@@ -5,12 +5,12 @@ import almerti.egline.data.repository.FolderRepository
 import almerti.egline.data.repository.UserRepository
 import almerti.egline.data.source.network.NetworkApi
 import almerti.egline.data.source.network.model.UserLogin
+import android.util.Log
 import androidx.datastore.core.DataStore
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
-import java.util.logging.Logger
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
@@ -27,9 +27,9 @@ class UserRepositoryImpl @Inject constructor(
         try {
             val user = userDataStore.data.first()
             val networkUser = userToNetworkUser(user)
+
             val savedBooks = JsonObject()
             val gson = Gson()
-
             folderRepository.getAll().collect {folderList ->
                 folderList.forEach {
                     savedBooks.addProperty(it.folderName, gson.toJson(it.bookIds))
@@ -37,43 +37,43 @@ class UserRepositoryImpl @Inject constructor(
             }
             networkUser.savedBooks = savedBooks
 
-            remoteApi.updateUser(networkUser.id, networkUser)
+            remoteApi.updateUser(userDataStore.data.first().id, networkUser)
         } catch (e : Exception) {
-            Logger.getGlobal().info(e.toString())
+            Log.e("UserRepositoryImpl", e.toString())
         }
     }
 
     override suspend fun get(userId : Int) : User? {
         try {
             val userData = remoteApi.getUser(userId)
-            if (userData.isSuccessful)
+            if (userData.isSuccessful && userData.body() != null) {
                 return networkUserToUser(userData.body()!!)
-            else
-                if (userDataStore.data.first().id == userId)
-                    return userDataStore.data.first()
-                else
-                    return null
+            }
         } catch (e : Exception) {
-            return userDataStore.data.first()
+            return null
         }
+        return if (userDataStore.data.first().id == userId)
+            userDataStore.data.first()
+        else
+            null
     }
 
     override suspend fun update(user : User) {
         userDataStore.updateData {
             user
         }
+        sendDataToServer()
     }
 
     override suspend fun register(user : User) : String {
         try {
-
             val answer = remoteApi.createUser(userToNetworkUser(user))
             if (answer.isSuccessful) {
                 login(user.email, user.password!!)
                 return "OK"
             } else return answer.errorBody()?.string() ?: "No response"
         } catch (e : Exception) {
-            Logger.getGlobal().info(e.toString())
+            Log.e("UserRepositoryImpl", e.toString())
             return "No response"
         }
     }
@@ -81,8 +81,12 @@ class UserRepositoryImpl @Inject constructor(
     override suspend fun login(email : String, password : String) : String {
         try {
             val response = remoteApi.login(UserLogin(email = email, password = password))
-            if (response.isSuccessful) {
+
+            if (response.isSuccessful && response.body() != null) {
                 val user = networkUserToUser(response.body()!!)
+
+                folderRepository.saveFoldersJson(response.body()!!.savedBooks)
+
                 userDataStore.updateData {
                     user
                 }
@@ -92,9 +96,16 @@ class UserRepositoryImpl @Inject constructor(
                 return response.errorBody()?.string() ?: "No response"
             }
         } catch (e : Exception) {
-            Logger.getGlobal().info(e.toString())
+            Log.e("UserRepositoryImpl", e.toString())
             return "No response"
         }
+    }
+
+    override suspend fun logout() {
+        userDataStore.updateData {
+            User()
+        }
+        folderRepository.removeAll()
     }
 
     override suspend fun delete(userId : Int) : String {
@@ -114,21 +125,20 @@ class UserRepositoryImpl @Inject constructor(
         try {
             val networkUser = remoteApi.getUser(userDataStore.data.first().id)
             if (networkUser.isSuccessful && networkUser.body() != null) {
+
                 val user = networkUserToUser(networkUser.body()!!)
+                folderRepository.saveFoldersJson(networkUser.body()!!.savedBooks)
                 if (user != userDataStore.data.first())
                     userDataStore.updateData {
                         user
                     }
             }
         } catch (e : Exception) {
-            Logger.getGlobal().info(e.toString())
+            Log.e("UserRepositoryImpl", e.toString())
         }
     }
 
-    private suspend fun networkUserToUser(user : almerti.egline.data.source.network.model.User) : User {
-
-        folderRepository.saveFoldersJson(user.savedBooks)
-
+    private fun networkUserToUser(user : almerti.egline.data.source.network.model.User) : User {
         return User(
             id = user.id,
             email = user.email,
